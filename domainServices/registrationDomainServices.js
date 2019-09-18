d2c377bb2069840bb5c3b419fd4bd53828faf02b
bsabillon <38368514+bsabillon@users.noreply.github.com>
2019-09-18 01:34:32 -0600

    added validations

--- domainServices/registrationDomainServices.js
+++ domainServices/registrationDomainServices.js
@@ -18,6 +18,53 @@ var domainMethods = {
         })
     },   
 
+    isUserValid : (user) => {
+        return new Promise((resolve, reject) => {
+           if(this.isNameValid(user.userName) == true &&
+              this.isNameValid(user.userName) == true) 
+            {
+                resolve("Valid");
+            }else{
+                reject("Invalid");
+            }
+        })
+    },
+
+    isNameValid: (userName )=>{
+        if ( userName.match(/[^a-zA-Z ]+/)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    },
+
+    isLastnameValid: (userLastname )=>{
+        if ( userLastname.match(/[^a-zA-Z ]+/)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    },
+
+    isUserEmailValid: (userEmail )=>{
+        if ( userEmail.includes('@')){
+            return true;
+        }
+        else{
+            return false;
+        }
+    },
+
+    isUserPasswordValid: (userPassword )=>{
+        if ( userPassword.match(/^(?=.*\d)(?=(.*\W){2})(?=.*[a-zA-Z])(?!.*\s).{1,15}$/)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
 
 module.exports= domainMethods;
